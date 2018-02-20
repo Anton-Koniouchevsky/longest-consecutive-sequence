bd2af3ac7934edf38c8ccd59cc40fa6a92245b1c
Anton-Koniouchevsky <anton.koniouchevsky@gmail.com>
2018-02-20 09:48:41 +0300

    Add longest-consecutive-sequence implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let maxLength = 0;
+    let objects = {};
+    array.forEach(element => {
+        if (!objects[element]) {
+            if (objects[element - 1] && objects[element + 1]) {
+                objects[element] = objects[element - 1] + objects[element + 1] + 1;
+                changeLeftValues(element, objects[element]);
+                changeRightValues(element, objects[element]);
+            } else if (objects[element - 1]) {
+                objects[element] = objects[element - 1] + 1;
+                changeLeftValues(element, objects[element]);
+            } else if (objects[element + 1]) {
+                objects[element] = objects[element + 1] + 1;
+                changeLeftValues(element, objects[element]);
+            } else {
+                objects[element] = 1;
+            }
+
+            maxLength = maxLength > objects[element] ? maxLength : objects[element];
+        }
+    });
+
+    return maxLength;
+
+    function changeLeftValues(index, value) {
+        while (objects[--index]) {
+            objects[index] = value;
+        }
+    }
+
+    function changeRightValues(index, value) {
+        while (objects[++index]) {
+            objects[index] = value;
+        }
+    }
 }
